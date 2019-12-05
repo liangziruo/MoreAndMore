90e3e40b6df31680ab59f8231b9ea20560c773b7
凉若 <983779411@qq.com>
2019-12-05 23:45:50 +0800

    1

---
+++ src/main/java/com/lay/LiveDataApplication.java
@@ -0,0 +1,18 @@
+package com.lay;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ * @date 2019/12/3
+ */
+@SpringBootApplication
+public class LiveDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveDataApplication.class);
+    }
+
+}
